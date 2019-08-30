a7a37af1cf873f9704c3c736817cd3ee05394730
ioanaMMT <ioana2morosanu@gmail.com>
2019-08-30 17:47:17 +0100

    My attempt to find the absolute difference

---
+++ difference.py
@@ -0,0 +1,41 @@
+
+A = [3, 1, 2, 4, 3]
+B = []
+
+A[0] = 3
+A[1] = 1
+A[2] = 2
+A[3] = 4
+A[4] = 3
+
+def difference(x, y):
+	return abs(x - y)
+
+for number in A
+	a = A[0]
+	b = A[1] + A[2] + A[3] + A[4]
+
+B += difference(a, b)
+
+for number in A
+	c = A[0] + A[1]
+	d = A[2] + A[3] + A[4]
+
+B += difference(c, d)
+
+for number in A 
+	e = A[0] + A[1] + A[2]
+	f = A[3] + A[4]
+
+B += difference(e, f)
+
+for number in A
+	g = A[0] + A[1] + A[2] + A[3]
+	h = A[4]
+
+B += difference(g, h)
+
+B.sort()
+	print(B[0])
+
+
