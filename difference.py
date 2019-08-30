40631c550fb5bfa9b102b50eccd7c427cfa91114
drapello <demetrius.rapello@gmail.com>
2019-08-30 23:07:20 +0200

    correction in sintaxe and list operator

--- difference.py
+++ difference.py
@@ -2,40 +2,41 @@
 A = [3, 1, 2, 4, 3]
 B = []
 
-A[0] = 3
-A[1] = 1
-A[2] = 2
-A[3] = 4
-A[4] = 3
+# Here you don't need to do this cause the statement A = [3, 1, 2, 4, 3] will do exatly the same!
+# A[0] = 3
+# A[1] = 1
+# A[2] = 2
+# A[3] = 4
+# A[4] = 3
 
 def difference(x, y):
 	return abs(x - y)
 
-for number in A
+for number in A:
 	a = A[0]
 	b = A[1] + A[2] + A[3] + A[4]
 
-B += difference(a, b)
+B.append(difference(a, b))
 
-for number in A
+for number in A:
 	c = A[0] + A[1]
 	d = A[2] + A[3] + A[4]
 
-B += difference(c, d)
+B.append(difference(c, d))
 
-for number in A 
+for number in A: 
 	e = A[0] + A[1] + A[2]
 	f = A[3] + A[4]
 
-B += difference(e, f)
+B.append(difference(e, f))
 
-for number in A
+for number in A:
 	g = A[0] + A[1] + A[2] + A[3]
 	h = A[4]
 
-B += difference(g, h)
+B.append(difference(g, h))
 
 B.sort()
-	print(B[0])
+print(B[0])
 
 
